d5e0d9c9ad7c99f392b7a7b317ab04d3783bdc7e
kschipulring <karl@3ringprototype.com>
2018-06-07 18:00:44 -0400

    updated dusk test

--- video-uploader/tests/Browser/ExampleTest.php
+++ video-uploader/tests/Browser/ExampleTest.php
@@ -14,7 +14,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 use Laravel\Dusk\TestCase as AbstractDuskTestCase;
 
-
+use Illuminate\Foundation\Auth\User;
 
 class ExampleTest extends DuskTestCase
 {
@@ -31,14 +31,41 @@ class ExampleTest extends DuskTestCase
         });
     }
 
-    public function testBasicLogin(){
+    public function testGuiLogin(){
         $this->browse(function (Browser $browser) {
             $browser->visit( '/login' )
-			->type('email', 'kschipul@yahoo.com')
-			->type('password', 'abc123')
-			->press('login_submit')
-			->assertPathIs('/home')
-	        ->pause(10000);
+            ->type('email', 'kschipul@yahoo.com')
+            ->type('password', 'abc123')
+            ->press('login_submit')
+            ->assertPathIs('/home');
+        });
+    }
+
+    public function testSimpleLogin(){
+        $this->browse(function ($first, $second) {
+            $first->loginAs(User::find(1))
+                  ->visit('/home')
+                  ->logout();
+        });
+    }
+
+    public function testGuiRegister(){
+        $this->browse(function (Browser $browser) {
+
+            $str = 'abcdefghijklmnopqrstuvwxyz1234567890';
+            $shuffled = str_shuffle($str);
+
+            $str2 = 'abcdefghijklmnopqrstuvwxyz';
+            $shuffled2 = str_shuffle($str2);
+
+            $browser
+            ->visit( '/register' )
+            ->type('name', 'Random User - ' . str_shuffle($str))
+            ->type('email', "{$shuffled}@{$shuffled2}.com")
+            ->type('password', 'abc123')
+            ->type('password_confirmation', 'abc123')
+            ->press('register_submit')
+            ->assertPathIs('/registered');
         });
     }
 }
